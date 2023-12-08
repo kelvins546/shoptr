f0e8e4eeb62f0608fba1ed7baa47ef7c038496b0
kelvin <kken5848@gmail.com>
2023-12-08 15:56:18 +0800

    first commit

---
+++ datatry/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datatry
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 instance;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            instance = this;
+      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+
+            Form2.instance.lab1.Text = textBox2.Text;
+            Form2.instance.lab2.Text = textBox3.Text;
+            Form2.instance.lab3.Text = textBox4.Text;
+            Form2.instance.lab4.Text = textBox1.Text;
+
+        }
+    }
+}
---
+++ datatry/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datatry
+{
+    public partial class Form2 : Form
+    {
+        public static Form2 instance;
+        public Label lab1,lab2,lab3,lab4;
+        public Form2()
+        {
+            InitializeComponent();
+            instance = this;
+            lab1 = label1;
+            lab2 = label2;
+            lab3 = label3;
+            lab4 = label4;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
